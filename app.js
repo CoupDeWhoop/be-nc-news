129a60f38139e77d79636021ee91b70326ecb24d
CoupDeWhoop <greenland.david@gmail.com>
2023-10-02 14:02:28 +0100

    after git problems

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const { getTopics } = require("./controllers/topics.controller.js");
 
 app.get('/api/topics', getTopics);
 
+app.all('/*', (req, res, next) => {
+    res.status(404).send({msg: 'path not found'})
+})
+
 module.exports = app;
